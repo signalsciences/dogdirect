af2f5eb768f59908c4150d9c54698de6d2f19384
nickg <nickg@signalsciences.com>
2018-09-21 12:20:33 -0700

    a generic task scheduler to flush and close metrics

---
+++ periodic.go
@@ -0,0 +1,60 @@
+package dogdirect
+
+import (
+	"time"
+)
+
+// FlushCloser is similar to a io.WriteCloser
+type FlushCloser interface {
+	Flush() error
+	Close() error
+}
+
+// Periodic handles flushing data periodically
+type Periodic struct {
+	client FlushCloser
+	stop   chan struct{}
+}
+
+// NewPeriodic task create a new ticket to flush data at regular intervals
+func NewPeriodic(client FlushCloser, duration time.Duration) *Periodic {
+	c := &Periodic{
+		client: client,
+	}
+	go c.watch(duration)
+	return c
+}
+
+func (p *Periodic) watch(duration time.Duration) {
+	ticker := time.NewTicker(duration)
+
+	for {
+		select {
+		case <-ticker.C:
+			// TODO error is squashed
+			if err := p.Flush(); err != nil {
+				// TODO: need call out
+			}
+		case <-p.stop:
+			ticker.Stop()
+			return
+		}
+	}
+}
+
+// Flush causes data to be written out
+func (p *Periodic) Flush() error {
+	return p.client.Flush()
+}
+
+// Close stops the ticket and closes the client
+func (p *Periodic) Close() error {
+	if p == nil {
+		return nil
+	}
+	select {
+	case p.stop <- struct{}{}:
+	default:
+	}
+	return p.client.Close()
+}
